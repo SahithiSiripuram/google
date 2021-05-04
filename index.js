d3161fb5b4e6d868dd497bde21d0e5ad7a5c6cc4
Sahithi Siripuram <sahithisiripuram1001@gmail.com>
2021-05-05 04:51:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app=express();
 app.use(express.static(__dirname+'/frontend'));
 app.get('/',function(req,res){
-    res.sendFile(__dirname+"/frontend/home.html");
+    res.sendFile(__dirname+"/frontend/index.html");
 })
 var port=process.env.PORT || 3000;
 app.listen(port,function(){
